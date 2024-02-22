d19ae1b93211a64cff8523a01977083b1543aae4
Marcos Acosta <macosta1625@gmail.com>
2024-02-22 09:12:55 -0500

    Replace img with next/Image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import styles from "./AboutMe.module.css";
 import globalStyles from "../../global.module.css";
+import DrummingImage from "/public/images/drumming.jpg";
+import Image from "next/image";
+
+const ABOUT_ME_IMAGE_STYLE = {
+  height: "70vh",
+  width: "auto",
+  borderRadius: "1vw",
+};
 
 const MUDD_URL =
   "https://www.google.com/maps/place/Harvey+Mudd+College/@34.1060906,-117.711218,17z/data=!3m1!4b1!4m6!3m5!1s0x80c331b99201d84f:0x24fab94d0a391365!8m2!3d34.1060906!4d-117.7086431!16zL20vMDNsY3Y?entry=ttu";
@@ -12,10 +20,11 @@ export default function AboutMe() {
     <div className={styles.aboutMeContainer}>
       <div className={styles.verticalAligner}>
         <div className={styles.aboutMeContent}>
-          <img
-            className={styles.aboutMeImage}
-            src="images/drumming.jpg"
+          <Image
+            style={ABOUT_ME_IMAGE_STYLE}
+            src={DrummingImage}
             alt="a picture of me drumming"
+            fill={false}
           />
           <div className={styles.aboutMeText}>
             <div className={styles.aboutMeTitle}>About me</div>
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styles from "./Home.module.css";
+import Image from "next/image";
+import MuddkartImage from "/public/images/muddkart.jpg";
+
+const SELF_IMAGE_STYLE = {
+  width: "18vw",
+  height: "18vw",
+  borderRadius: "0.8vw",
+};
 
 export default function Home() {
   return (
     <div className={styles.homePageContainer}>
       <div className={styles.centerer}>
         <div className={styles.nameAndImageContainer}>
-          <img
-            src="/images/muddkart.jpg"
-            alt="a picture of me"
-            className={styles.selfImage}
-          />
+          <div className={styles.selfImage}>
+            <Image
+              src={MuddkartImage}
+              alt="a picture of me"
+              style={SELF_IMAGE_STYLE}
+            />
+          </div>
           <div className={styles.nameContainer}>marcos acosta</div>
         </div>
       </div>
